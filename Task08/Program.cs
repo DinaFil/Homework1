46df959745986ac1acb3e4ec5156e5ce401e59fe
Dina Filippova <dinafilippova@gmail.com>
2022-12-05 12:00:12 +0700

    Task 08

---
+++ Task08/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 2; 
+if (n < i) Console.WriteLine("Нет чисел для отображения");
+while (i <= n)
+{
+Console.WriteLine(i); 
+i = i + 2;
+}
