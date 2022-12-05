532fe1dcc0b65ea5c791108f1d76fa917eb76016
Dina Filippova <dinafilippova@gmail.com>
2022-12-05 09:54:57 +0700

    Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+int max = 0;
+if (number1 >= number2)
+{
+    max = number1;
+}
+else
+{
+max = number2;
+}
+
+if (number3 >= max)
+{
+Console.WriteLine("Максимальное число: "+ +number3);
+}
+
+else
+{
+    Console.WriteLine("Максимальное числое: "+ +max);
+}
