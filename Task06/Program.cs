1d61b8129183219770aac62c38ea7ee17df3cd40
Dina Filippova <dinafilippova@gmail.com>
2022-12-05 11:18:24 +0700

    even number LAST VERSION

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -1,30 +1,5 @@
 ﻿Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
-bool positive = true;
-
-if (number >= 0) positive = true;
-else positive = false;
-
-switch(positive)
-
-   { 
-    case true:
-    if (number % 2 == 0) Console.WriteLine("Введено четное число");
-    else Console.WriteLine("Введено нечетное число");
-    break;
-
-    case false:
-    if (number % 2 == -1) Console.WriteLine("Введено нечетное число");
-    else Console.WriteLine("Введено четное число");
-    break;
-    default: Console.WriteLine("Число не введено");
-}
-
-
-   
-    
-  
-
-
-
+if (number % 2 == 0) Console.WriteLine("Введено четное число");
+else Console.WriteLine("Введено нечетное число");
 
